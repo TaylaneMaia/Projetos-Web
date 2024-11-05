390dc15a4877697388e10e21fb744266408b634f
Taylane Maia <142900247+taylane-pixel@users.noreply.github.com>
2024-11-05 12:55:36 -0300

    Projeto_Site_De_Adoção_Pet
    Criação de um site responsivo de uma ONG de adoção de pets.

---
+++ Adoção_Pet/src/scripit.js
@@ -0,0 +1,8 @@
+// Fazer o landing page levar para o site ao clicar no botão
+
+const btn = document.querySelector('#btn-page');
+
+btn.addEventListener('click', () =>{
+    window.location.href = 'AmorEmPatas.html';
+});
+
---
+++ Adoção_Pet/src/site_principal.js
@@ -0,0 +1,7 @@
+// SCRIPT PARA O SITE AMOR EM PATAS
+
+// FUNÇÃO MENU MOBILE
+
+function toggleMenu() {
+    document.querySelector('#nav-link').classList.toggle('open');
+  };
